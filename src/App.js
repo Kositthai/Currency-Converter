7bb07a52d8bb0dffa66ad5371a4eea7552444d5c
Kositthai <vkositthai@outlook.com>
2023-04-29 14:07:44 +0200

    Added some styles, store api key in dotenv, adjust calculate currency

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import CurrencyAmount from "./component/CurrencyAmount";
 import SelectCurrencyFrom from "./component/SelectCurrencyFrom";
 import SelectCurrencyTo from "./component/SelectCurrencyTo";
-import ConversionResult from "./component/ConvensionResult";
+import ConversionResult from "./component/ConversionResult";
+import { CurrencyContext } from "./context/CurrencyContext";
+import { useContext } from "react";
+import "./styles/App.css";
 
 function App() {
+  const { fetchState } = useContext(CurrencyContext);
+
+  if (fetchState.error) {
+    return <p className="Error-Text">Something went wrong! Try again later</p>;
+  }
+
+  if (fetchState.loading) {
+    return <i className="fas fa-spinner fa-spin"></i>;
+  }
+
   return (
     <div className="App">
-      <h1>Currency Converter</h1>
-      <CurrencyAmount />
-      <SelectCurrencyFrom />
-      <SelectCurrencyTo />
-      <ConversionResult />
+      <div className="App-Content-Wrapper">
+        <h1>Currency Converter</h1>
+        <div className="App-Contents">
+          <div className="Currency-Form">
+            <CurrencyAmount />
+            <SelectCurrencyFrom />
+            <SelectCurrencyTo />
+          </div>
+          <ConversionResult />
+        </div>
+      </div>
     </div>
   );
 }
